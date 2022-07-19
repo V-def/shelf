fbd7d76ff74e5948a57f8b5e9d50405b7c702fc7
V-def <valentin.cassayre@gmail.com>
2022-07-19 20:43:52 +0200

    Added error pages

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, url_for, redirect
 app = Flask(__name__)
 app.config.from_object('config')
 
+# Main pages
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,7 +13,18 @@ def index():
 def search():
     return render_template('search.html')
 
-# pwa
+# Errors
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', error_message="Mince ! La page que vous avez cherché n'existe pas...", error_log=e)
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('error.html', error_message="Mince ! Il y a eu une erreur interne...", error_log=e)
+
+# PWA
+
 @app.route('/sw.js', methods=['GET'])
 def sw():
     return app.send_static_file('js/sw.js')
